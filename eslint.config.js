c5b235514aac11b2c861683ea813efae0a30ad5b
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2025-08-26 20:42:47 -0400

    Rework linting

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "eslint/config"
+import eslintJs from "@eslint/js"
+import typescriptEslint from "typescript-eslint"
+import eslintPluginVue from "eslint-plugin-vue"
+import globals from "globals"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+
+export default defineConfig([
+  { ignores: ["**/*.d.ts", "dist"] },
+  {
+    files: ["**/*.{js,ts,vue}"],
+    extends: [
+      eslintJs.configs.recommended,
+      typescriptEslint.configs.recommended,
+      eslintPluginVue.configs["flat/recommended"]
+    ],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        parser: typescriptEslint.parser
+      }
+    },
+    rules: {
+      "vue/multi-word-component-names": "off"
+    }
+  },
+  eslintConfigPrettier
+])
