878e8d5804435ff8e8959b092074c15aa4235d3e
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2025-12-09 21:27:43 -0500

    Update banner message again

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,7 +2,7 @@
   <div class="w-full bg-purple-100 text-dark px-6 py-4 shadow-md mb-6">
     <div class="z-20">
       <p class="font-semibold">
-        🎉 We are looking for new members in 2026. Come join us next semester!
+        🚀 Build the projects you wish existed. Come join us for spring 2026! We look forward to seeing you! 🤩
       </p>
     </div>
   </div>
