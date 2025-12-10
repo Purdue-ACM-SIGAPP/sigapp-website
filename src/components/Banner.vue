c58fc0bd3a25e11bb92e7288aadbfc270bda6211
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2025-12-09 20:28:34 -0500

    Add back the banner and change the message

---
+++ src/components/Banner.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-full bg-purple-100 text-dark px-6 py-4 shadow-md mb-6">
+    <div class="z-20">
+      <p class="font-semibold">
+        🎉 We are looking for new members in 2026. Come join us next semester!
+      </p>
+    </div>
+  </div>
+</template>
